e61efedc19059183906eea04d92469a2ac31cc21
Quentin Vanhauteghem <quentvht@hotmail.com>
2024-11-18 01:23:20 +0100

    fix morph progress

--- src/js/managers/MeshManager.js
+++ src/js/managers/MeshManager.js
@@ -2,7 +2,6 @@ import CubeMesh from "../meshes/CubeMesh";
 import CylinderMesh from "../meshes/CylinderMesh";
 import DrawingMesh from "../meshes/DrawingMesh";
 import TextMesh from "../meshes/TextMesh";
-import * as THREE from 'three';
 
 const meshes = {
     drawing: () => DrawingMesh,
@@ -54,7 +53,7 @@ export default class MeshManager {
                 const borders = this._createBorder(this.properties.width, this.properties.height);
                 this.properties.drawings = [ borders, this.properties.drawings[0] ];
                 await this.nextMesh('drawing');
-                setTimeout(() => this.nextMesh('drawing'), 1000 );
+                setTimeout(() => this.nextMesh('drawing'), 3000 );
             } else {
                 await this.nextMesh('drawing');
             }
@@ -149,52 +148,62 @@ export default class MeshManager {
     }
 
     updateMorph() {
-        if (!this.animateMorph) return;
-    
-        this.morphProgress += 0.01;
-          
-        if ( this.morphProgress <= 1/3 ) {
-          const shapesTarget = this.objects[1].children;
-          let k = 0;
-          let j = 0;
-          const getNextTargetPoints = () => {
-              if ( k >= shapesTarget.length ) {
-                return null;
-              }
-              let pos = shapesTarget[k].geometry.attributes.position.array;
-              if ( j >= pos.length ) {
-                j = 0; 
-                k++;
-                return getNextTargetPoints();
-              }
-              const pts = [pos[j], pos[j+1], pos[j+2]];
-              j = j + 3;
-              return pts;
-          }
-          
-          this.objects[0].children.forEach((shape1, index) => {
-            const pos1 = shape1.geometry.attributes.position.array;
-            for (let i = 0; i < pos1.length; i = i + 3) {
-              const targetPoints = getNextTargetPoints();
-              if ( !targetPoints ) continue;
-              
-              pos1[i] = pos1[i] * (1 - this.morphProgress) + targetPoints[0] * this.morphProgress;
-              pos1[i+1] = pos1[i+1] * (1 - this.morphProgress) + targetPoints[1] * this.morphProgress;
-              pos1[i+2] = pos1[i+2] * (1 - this.morphProgress) + targetPoints[2] * this.morphProgress;
+        if (!this.animateMorph) return;      
+
+        if ( this.morphProgress < 1 ) { 
+            this.morphProgress += 0.02;
+            const morphProgress = 3 * Math.pow(this.morphProgress, 2) - 2 * Math.pow(this.morphProgress, 3);
+            function interpolate(start, end) { 
+                return start * (1 - morphProgress) + end * morphProgress; 
+            } 
+
+            const shapesTarget = this.objects[1].children;
+            let k = 0;
+            let j = 0;
+
+            const getNextTargetPoints = () => {
+                if ( k >= shapesTarget.length ) {
+                    return null;
+                }
+                let pos = shapesTarget[k].geometry.attributes.position.array;
+                if ( j >= pos.length ) {
+                    j = 0; 
+                    k++;
+                    return getNextTargetPoints();
+                }
+                const pts = [pos[j], pos[j+1], pos[j+2]];
+                j = j + 3;
+
+                return pts;
             }
-    
-            shape1.geometry.attributes.position.needsUpdate = true;
-          });  
-        } else {
-          //console.log('morph done');
-    
-          this.animateMorph = false;
-          this.morphProgress = 0;
-    
-          this.holderObjects?.clear();
-          this.objects[0] = this.objects[1];
-          this.objects[1] = null;
-          this.holderObjects.add(this.objects[0]);
+
+            if (!this.morphInitialPositions) { 
+                this.morphInitialPositions = this.objects[0].children.map(shape => { return shape.geometry.attributes.position.array.slice(); }); 
+            }
+            
+            this.objects[0].children.forEach((shape1, index) => {
+                const initPos1 = this.morphInitialPositions[index];
+                const pos1 = shape1.geometry.attributes.position.array;
+                for (let i = 0; i < pos1.length; i = i + 3) {
+                    const targetPoints = getNextTargetPoints();
+                    if ( !targetPoints ) continue;
+                    
+                    pos1[i] = interpolate(initPos1[i], targetPoints[0]);
+                    pos1[i+1] = interpolate(initPos1[i+1], targetPoints[1]);
+                    pos1[i+2] = interpolate(initPos1[i+2], targetPoints[2]);
+                }
+        
+                shape1.geometry.attributes.position.needsUpdate = true;
+            }); 
+        } else {    
+            this.animateMorph = false;
+            this.morphProgress = 0;
+            this.morphInitialPositions = null;
+        
+            this.holderObjects?.clear();
+            this.objects[0] = this.objects[1];
+            this.objects[1] = null;
+            this.holderObjects.add(this.objects[0]);
         }
     }
 
@@ -207,7 +216,7 @@ export default class MeshManager {
             this.morphTimeout = setTimeout(() => {
               this.updateMorph();
               this.morphTimeout = null;
-            }, 24 );
+            }, 24);
         }
     }    
 }
