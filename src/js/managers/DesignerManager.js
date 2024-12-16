cf24994c2f0cc1feb70ca22cb05a196bfd0454c9
Quentin Vanhauteghem <quentvht@hotmail.com>
2024-12-16 18:59:31 +0100

    visualizer: copy button

--- src/js/managers/DesignerManager.js
+++ src/js/managers/DesignerManager.js
@@ -14,6 +14,14 @@ export default class DesignerManager {
             console.log(url)
             document.location.href = url;
         })
+        const designerGenerate = document.getElementById('designerGenerate');
+        designerGenerate.addEventListener('click', () => {
+            const url = this.generateUrl();
+            navigator.clipboard.writeText(JSON.stringify(
+                url.replaceAll('192.168.1.84:8585', 'storageapi-rest:8585')
+                    .replaceAll('localhost:8586', 'videosvisualizer:8586')
+            ));
+        });
 
         this.init(this.options);
         this.root.style.display = "block";
@@ -39,7 +47,7 @@ export default class DesignerManager {
             <textarea name="config"></textarea>
         </div>
         <div>
-            <button id="designerReload">Reload</button>
+            <button id="designerReload">Reload</button><button id="designerGenerate">Copy</button>
         </div>
         `
     }
