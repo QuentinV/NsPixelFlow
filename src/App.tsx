a7c5616bcd849189e856ac4f4f5c1c06d28da71b
Quentin Vanhauteghem <quentvht@hotmail.com>
2025-04-20 21:39:44 +0200

    toast context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { createContext, useRef } from 'react';
 import 'primereact/resources/themes/lara-dark-amber/theme.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 import './theme.css';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { InboxPage } from './pages/Inbox';
+import { EditorPage } from './pages/Editor';
+import { Toast, ToastMessage } from 'primereact/toast';
+
+const ToastContext = createContext<(toastMessage: ToastMessage) => void>(
+    () => {}
+);
 
 export const App = () => {
+    const toast = useRef<Toast>(null);
+
+    const sendToast = (toastMessage: ToastMessage) => {
+        toast.current?.show(toastMessage);
+    };
+
     return (
-        <div className="App">
-            <h1>Hello World</h1>
-            <p>This is a simple React app.</p>
-        </div>
+        <>
+            <ToastContext.Provider value={sendToast}>
+                <HashRouter>
+                    <Routes>
+                        <Route index element={<InboxPage />} />
+                        <Route
+                            path="/project/:projectId"
+                            element={<EditorPage />}
+                        />
+                    </Routes>
+                </HashRouter>
+                <Toast ref={toast} position="top-right" className="w-25rem" />
+            </ToastContext.Provider>
+        </>
     );
 };
