e3de95beb015e00114eb478a161ad04777c36d96
Quentin Vanhauteghem <quentvht@hotmail.com>
2025-04-21 00:55:51 +0200

    inbox

--- src/pages/Inbox/index.tsx
+++ src/pages/Inbox/index.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Button } from 'primereact/button';
 
 export const InboxPage = () => {
-    return <div></div>;
+    return (
+        <div className="flex align-items-center gap-4 justify-content-center w-full h-full flex-wrap">
+            <div>
+                <img src="logo-transparent.png" alt="Logo" />
+            </div>
+            <div>
+                <Button
+                    icon="pi pi-folder-plus"
+                    label="New project"
+                    size="large"
+                />
+            </div>
+        </div>
+    );
 };
